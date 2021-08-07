c03f4f0b6a4a3fe8d87677f69d856c5d47ced11d
Behzad <bch15@live.com>
2021-08-07 11:51:50 +0430

    modified offline list

--- src/pages/main/components/my-library/components/offline-list/OffilineList.js
+++ src/pages/main/components/my-library/components/offline-list/OffilineList.js
@@ -1,9 +1,49 @@
+import React, { useEffect, useState } from "react";
 import useStyles from "./styles";
+import { Grid } from "@material-ui/core";
+import { toast } from "material-react-toastify";
+import CardProduct from "../../../../../../components/card-product/CardProduct";
+import Loading from "../../../../../../components/Loading/Loading";
+import { getMoreSection } from "../../../../../../api/api_content";
 
-const OffilineList = () => {
+const OfflineList = () => {
   const classes = useStyles();
+  const [books, setBooks] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
-  return <div style={{ color: "#fff" }}>blobla</div>;
+  // ketabhaye kharidari shode:
+  useEffect(() => {
+    getMoreSection((isOk, data) => {
+      if (!isOk) return toast.warning(data.message);
+      setBooks(data);
+      setLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        <Grid
+          container
+          item
+          justifyContent={"center"}
+          className={classes.cardContainer}
+        >
+          {books.map((item) => {
+            return (
+              <>
+                <CardProduct item={item} />
+              </>
+            );
+          })}
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
-export default OffilineList;
+export default OfflineList;
