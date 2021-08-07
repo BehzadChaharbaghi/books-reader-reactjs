3dafb806971ff8bbaa5e190e595d0823fa098997
Behzad <bch15@live.com>
2021-08-07 11:58:49 +0430

    modified MyBooksList list

--- src/pages/main/components/my-library/components/my-books-list/MyBooksList.js
+++ src/pages/main/components/my-library/components/my-books-list/MyBooksList.js
@@ -1,49 +1,46 @@
 import React, { useEffect, useState } from "react";
 import useStyles from "./styles";
-import { Button, Grid } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import { toast } from "material-react-toastify";
 import CardProduct from "../../../../../../components/card-product/CardProduct";
+import Loading from "../../../../../../components/Loading/Loading";
+import { getMoreSection } from "../../../../../../api/api_content";
 
 const MyBooksList = () => {
   const classes = useStyles();
   const [books, setBooks] = useState([]);
   const [isLoading, setLoading] = useState(true);
 
-  //ketabhaye kharidari shode:
-  // useEffect(() => {
-  //   getMoreSection((isOk, data) => {
-  //     if (!isOk) return toast.warning(data.message);
-  //     setBooks(data);
-  //     setLoading(false);
-  //   });
-  // }, []);
-  //
-  // if (isLoading) {
-  //   return <Loading />;
-  // }
+  // ketabhaye kharidari shode:
+  useEffect(() => {
+    getMoreSection((isOk, data) => {
+      if (!isOk) return toast.warning(data.message);
+      setBooks(data);
+      setLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
-    <div>
+    <div className={classes.root}>
       <Grid container>
-        <Grid container item xs={12}>
-          <Grid item>
-            <Button>آفلاین شده</Button>
-          </Grid>
-          <Grid item>
-            <Button>کتاب های من</Button>
-          </Grid>
-          <Grid item>
-            <Button>نشان شده</Button>
-          </Grid>
+        <Grid
+          container
+          item
+          justifyContent={"center"}
+          className={classes.cardContainer}
+        >
+          {books.map((item) => {
+            return (
+              <>
+                <CardProduct item={item} />
+              </>
+            );
+          })}
         </Grid>
-
-        {books.map((item) => {
-          return (
-            <>
-              <CardProduct item={item} />
-            </>
-          );
-        })}
       </Grid>
     </div>
   );
--- src/pages/main/components/my-library/components/my-books-list/styles.js
+++ src/pages/main/components/my-library/components/my-books-list/styles.js
@@ -1,70 +1,11 @@
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "80%",
-    margin: "0 auto",
-    textAlign: "center",
-    color: "#ddd",
-  },
-  container: {
-    display: "flex",
-    marginTop: "2rem",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-    [theme.breakpoints.up("lg")]: {
-      flexDirection: "row",
-      justifyContent: "space-around",
-      width: "100%",
+  root: { marginTop: "2rem" },
+  cardContainer: {
+    [theme.breakpoints.up("sm")]: {
+      justifyContent: "flex-start",
     },
-    [theme.breakpoints.down("sm")]: {
-      flexDirection: "column",
-      width: "98%",
-      justifyContent: "space-around",
-    },
-  },
-  card: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignContent: "center",
-    alignItems: "center",
-    background: "unset",
-    color: "#fff",
-    width: "15rem",
-    height: "16rem",
-    // margin: "0 1rem",
-    borderRadius: 15,
-    "& a": {
-      textDecoration: "unset",
-      width: "100%",
-    },
-    [theme.breakpoints.up("lg")]: {
-      flexDirection: "row",
-      justifyContent: "space-around",
-    },
-    [theme.breakpoints.down("sm")]: {
-      flexDirection: "column",
-      justifyContent: "space-around",
-    },
-  },
-  media: {
-    height: "14rem",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-  },
-  title: {
-    color: "#ddd",
-    textOverflow: "ellipsis",
-    overflow: "hidden",
-    whiteSpace: "nowrap",
-  },
-  desc: {
-    color: "#ddd",
   },
 }));
 
