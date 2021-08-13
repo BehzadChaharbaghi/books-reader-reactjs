f008b357164f078abe825d1f8084509cc6d8d925
Behzad <bch15@live.com>
2021-08-13 11:44:31 +0430

    app and routes modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import LandingPage from "./pages/landing page/LandingPage";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import AudioBook from "./pages/main/components/audio-book/AudioBook";
 import { ToastContainer } from "material-react-toastify";
 import Layout from "./components/layout/Layout";
@@ -19,66 +19,72 @@ const isLogin = () => !!localStorage.getItem("token");
 //Pub-Pri Component
 
 const PublicRoute = ({ component, ...props }) => {
-  // return (
-  //   <Route
-  //     {...props}
-  //     render={(props) => {
-  //       if (isLogin()) return <Redirect to="/" />;
-  //       else {
-  //         return React.createElement(component, props);
-  //       }
-  //     }}
-  //   />
-  // );
+  return (
+    <Route
+      {...props}
+      render={(props) => {
+        if (isLogin()) return <Redirect to="/" />;
+        else {
+          return React.createElement(component, props);
+        }
+      }}
+    />
+  );
 };
 const PrivateRoute = ({ render, ...props }) => {
-  // return (
-  //   <Route
-  //     {...props}
-  //     render={(props) => {
-  //       if (isLogin()) return render(props);
-  //       else return <Redirect to={"/"} />;
-  //     }}
-  //   />
-  // );
+  return (
+    <Route
+      {...props}
+      render={(props) => {
+        if (isLogin()) return render(props);
+        else return <Redirect to={"/index"} />;
+      }}
+    />
+  );
 };
 
 const App = () => {
   return (
     <>
-      {/*<BrowserRouter>*/}
-      {/*  <Switch>*/}
-      {/*<PublicRoute path={"/"} component={LandingPage} />*/}
-      {/*<PrivateRoute*/}
-      {/*  path={"/"}*/}
-      {/*  render={() => (*/}
       <BrowserRouter>
         <Switch>
-          <Route exact path={"/"} component={LandingPage} />
-          <LayoutProvider>
-            <ProductProvider>
-              <Layout>
-                <Main>
-                  <Route path={"/index"} component={MainPage} />
-                  <Route path={"/AudioBook"} component={AudioBook} />
-                  <Route path={"/BookDetail/:id"} component={BookDetail} />
-                  <Route
-                    path={"/SubscriptionTypes"}
-                    component={SubscriptionTypes}
-                  />
-                  <Route path={"/IncreaseCredit"} component={IncreaseCredit} />
-                  <Route path={"/MyLibrary"} component={MyLibrary} />
-                </Main>
-                <Route path={"/UserInfo"} component={UserInfo} />
-              </Layout>
-            </ProductProvider>
-          </LayoutProvider>
+          <PublicRoute path={"/"} component={LandingPage} />
+          <PrivateRoute
+            path={"/"}
+            render={() => (
+              <BrowserRouter>
+                <Switch>
+                  <Route exact path={"/"} component={LandingPage} />
+                  <LayoutProvider>
+                    <ProductProvider>
+                      <Layout>
+                        <Main>
+                          <Route path={"/index"} component={MainPage} />
+                          <Route path={"/AudioBook"} component={AudioBook} />
+                          <Route
+                            path={"/BookDetail/:id"}
+                            component={BookDetail}
+                          />
+                          <Route
+                            path={"/SubscriptionTypes"}
+                            component={SubscriptionTypes}
+                          />
+                          <Route
+                            path={"/IncreaseCredit"}
+                            component={IncreaseCredit}
+                          />
+                          <Route path={"/MyLibrary"} component={MyLibrary} />
+                        </Main>
+                        <Route path={"/UserInfo"} component={UserInfo} />
+                      </Layout>
+                    </ProductProvider>
+                  </LayoutProvider>
+                </Switch>
+              </BrowserRouter>
+            )}
+          />
         </Switch>
       </BrowserRouter>
-      {/*      )}*/}
-      {/*    />*/}
-      {/*  </Switch>*/}
-      {/*</BrowserRouter>*/}
       <ToastContainer
         position="top-right"
         autoClose={3000}
